1269ee10c27871ae93bf7af6c7c00c42b24ac916
Alexander <car3651@yandex.ru>
2020-11-18 19:38:46 +0500

    1.0.0

--- src/app/cursormove.directive.ts
+++ src/app/cursormove.directive.ts
@@ -7,10 +7,12 @@ export class CursormoveDirective {
 
   constructor(private elementRef:ElementRef) { }
 
-  @HostListener('focus')
+  @HostListener('click')
   focus() {
     const input =  this.elementRef.nativeElement;
     const first_index = input.value.indexOf('_');
+    console.log(first_index);
     input.setSelectionRange(first_index,first_index);
+    // input.scrollLeft = input.scrollWidth;
   }
 }
